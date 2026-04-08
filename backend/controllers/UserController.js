99e9cc03bde4b77838f5b9507edf86372bd028fa
victoricoma <victorsena3010@gmail.com>
2026-04-08 12:48:53 -0300

    Testes do dominio e rotas com controllers

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,5 @@
+module.exports = class UserController {
+    static async register(req, res) {
+        res.json('Olá Get Pet')
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const UserRouters = require('./routers/UserRouters')
 
 const app = express()
 
 app.use(express.json())
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 
 app.use(express.static('public'))
+
+app.use('/users', UserRouters)
 
 app.listen(5000)
